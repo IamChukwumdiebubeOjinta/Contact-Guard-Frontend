e09e1af7840e64ea249a6f56827847750dfebde6
chukwumdiebube ojinta <chukwumdiebubeojinta@gmail.com>
2024-07-22 17:05:56 +0100

    added next auth middleware

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("hux-auth-token")?.value;
+
+  if (!token && !request.nextUrl.pathname.startsWith("/signin")) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
